a7ffea76992d790fc8915fd4bd4f2eb017870044
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-04 17:38:12 +0300

    Reverse String

---
+++ 10402.ReverseString/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace _10402.ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(input.Length);
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                sb.Append(input[i]);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
