2aad97af7734535409f01df6e09808a85a32c34d
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-04 18:02:04 +0300

    Correct Brackets Test

---
+++ 10403.CorrectBracketsTest/CorrectBracketsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _10403.CorrectBracketsTest
+{
+    class CorrectBracketsTest
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int check = 0;
+            string isCorrect = "Incorrect";
+
+            foreach (char item in input)
+            {
+                if (item == '(')
+                {
+                    check++;
+                }
+                else if (item == ')')
+                {
+                    check--;
+                    if (check < 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (check == 0)
+            {
+                isCorrect = "Correct";
+            }
+            Console.WriteLine(isCorrect);
+        }
+    }
+}
