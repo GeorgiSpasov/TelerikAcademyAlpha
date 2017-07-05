142af535a72a6771b1ec37c9fa5f1d7a644c231f
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-05 18:02:58 +0300

    Substring In Text

---
+++ 10404.SubstringInText/SubstringInText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _10404.SubstringInText
+{
+    class SubstringInText
+    {
+        static void Main(string[] args)
+        {
+            int occurrence;
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+            occurrence = Regex.Matches(text, pattern).Count;
+
+            Console.WriteLine(occurrence);
+        }
+    }
+}
