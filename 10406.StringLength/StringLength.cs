13992abd2d92985e8603136faa792b580430bc6e
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-06 21:40:32 +0300

    String Length

---
+++ 10406.StringLength/StringLength.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace _10406.StringLength
+{
+    class StringLength
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder result = new StringBuilder(20);
+            string input = Console.ReadLine();
+            result.Append(input)
+                  .Append(new string('*', 20 - input.Length));
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
