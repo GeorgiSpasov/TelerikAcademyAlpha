501e0bf3cbf31d7fbd455e0da8efc4ecf571a76a
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 04:26:08 +0300

    Series of Letters

---
+++ 10423.SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace _10423.SeriesOfLetters
+{
+    class SeriesOfLetters
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1] || i == input.Length - 2)
+                {
+                    result.Append(input[i]);
+                }
+            }
+            if (input[input.Length - 1] != input[input.Length - 2])
+            {
+                result.Append(input[input.Length - 1]);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
