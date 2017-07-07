646f53276bc5f4e08bd5b579c753b9b48f2e9995
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 21:56:43 +0300

    Compare Arrays

---
+++ 10102.CompareArrays/CompareArrays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _10102.CompareArrays
+{
+    class CompareArrays
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrayA = new int[n];
+            int[] arrayB = new int[n];
+            string areEqual = "Equal";
+            for (int i = 0; i < n; i++)
+            {
+                arrayA[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                arrayB[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arrayA[i]!=arrayB[i])
+                {
+                    areEqual = "Not equal";
+                    break;
+                }
+            }
+
+            Console.WriteLine(areEqual);
+        }
+    }
+}
