8be04683ec8c8f582cd106a5e12fe5954fc38bb3
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 23:11:44 +0300

    Maximal K Sum

---
+++ 10106.MaximalKSum/MaximalKSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10106.MaximalKSum
+{
+    class MaximalKSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] intArray = new int[n];
+            int maxSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(intArray);
+            for (int i = 0; i < k; i++)
+            {
+                maxSum += intArray[intArray.Length - 1 - i];
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
