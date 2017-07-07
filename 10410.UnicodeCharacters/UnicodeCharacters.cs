5caf6de2f81b7e0ad09183d32d09d986e5ea93f9
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 03:26:18 +0300

    Unicode Characters

---
+++ 10410.UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10410.UnicodeCharacters
+{
+    class UnicodeCharacters
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            foreach (char item in input)
+            {
+                Console.Write("\\u" + ((int)item).ToString("X").PadLeft(4, '0'));
+            }
+        }
+    }
+}
