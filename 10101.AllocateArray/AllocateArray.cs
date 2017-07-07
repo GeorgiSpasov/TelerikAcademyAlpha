28f7fc9099b7405eb0f3420c44f65b4da4ccec92
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 21:47:13 +0300

    Allocate Array

---
+++ 10101.AllocateArray/AllocateArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10101.AllocateArray
+{
+    class AllocateArray
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = i * 5;
+                Console.WriteLine(intArray[i]);
+            }
+        }
+    }
+}
