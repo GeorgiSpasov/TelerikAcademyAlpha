d65f82c63dbbedb6b01ae507030ebec45227c498
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 22:35:33 +0300

    Compare Char Arrays

---
+++ 10103.CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _10103.CompareCharArrays
+{
+    class CompareCharArrays
+    {
+        static void Main(string[] args)
+        {
+            string arrayA = Console.ReadLine();
+            string arrayB = Console.ReadLine();
+            int result = 0;
+
+            int minLength = arrayA.Length < arrayB.Length ? arrayA.Length : arrayB.Length;
+            for (int i = 0; i < minLength; i++)
+            {
+                if (arrayA[i]!=arrayB[i])
+                {
+                    result = arrayA[i] - arrayB[i];
+                    break;
+                }
+            }
+            if (result==0)
+            {
+                result = arrayA.Length - arrayB.Length;
+            }
+            Console.WriteLine(result < 0 ? "<" : result > 0 ? ">" : "=");
+
+        }
+    }
+}
