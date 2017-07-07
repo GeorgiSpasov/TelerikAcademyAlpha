f9902ea44270ea76a290b60008c365aa7fd1c22b
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-07 03:50:48 +0300

    Parse URL

---
+++ 10412.ParseURL/ParseURL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _10412.ParseURL
+{
+    class ParseURL
+    {
+        static void Main(string[] args)
+        {
+            string url = Console.ReadLine();
+            string protocol = GetProtocol(url);
+            string server = GetServer(url);
+            string resource = GetResource(url);
+            Console.WriteLine(protocol);
+            Console.WriteLine(server);
+            Console.WriteLine(resource);
+        }
+
+        public static string GetProtocol(string url)
+        {
+            int endIndex = url.IndexOf("://");
+            string protocol = url.Substring(0, endIndex);
+            return "[protocol] = " + protocol;
+        }
+
+        public static string GetServer(string url)
+        {
+            int protocolEndIndex = url.IndexOf("://")+3;
+            int serverIndex = url.IndexOf('/', protocolEndIndex);
+            string protocol = url.Substring(protocolEndIndex, serverIndex - protocolEndIndex);
+            return "[server] = " + protocol;
+        }
+
+        public static string GetResource(string url)
+        {
+            int protocolEndIndex = url.IndexOf("://") + 3;
+            int serverEndIndex = url.IndexOf('/', protocolEndIndex);
+            string resource = url.Substring(serverEndIndex);
+            return "[resource] = " + resource;
+        }
+    }
+}
