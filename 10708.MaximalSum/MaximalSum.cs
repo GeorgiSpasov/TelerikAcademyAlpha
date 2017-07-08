82a69b5b498991d1616576d2d9c0eaef529481d4
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-09 01:18:39 +0300

    Maximal Sum

---
+++ 10708.MaximalSum/MaximalSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10708.MaximalSum
+{
+    class MaximalSum
+    {
+        static void Main(string[] args)
+        {
+            // Kadane's algorithm
+            // https://en.wikipedia.org/wiki/Maximum_subarray_problem
+
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = new int[n];
+            int maxSoFar = 0;
+            int maxEndingHere = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+            
+            foreach (int num in intArray)
+            {
+                int x = num;
+                maxEndingHere = Math.Max(x, maxEndingHere + x);
+                maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+            }
+            Console.WriteLine(maxSoFar);
+        }
+    }
+}
