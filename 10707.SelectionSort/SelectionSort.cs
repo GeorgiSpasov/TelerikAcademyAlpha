1b12130056d5e4b654f0d0ce0c422d7bb58062ea
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-09 00:10:46 +0300

    Selection Sort

---
+++ 10707.SelectionSort/SelectionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10707.SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> intList = new List<int>(n); ;
+            int[] finalArray = new int[n];
+            int min = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                intList.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (min > intList[j])
+                    {
+                        min = intList[j];
+                    }
+                }
+                intList.Remove(min);
+                finalArray[i] = min;
+                min = int.MaxValue;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(finalArray[i]);
+            }
+
+        }
+    }
+}
