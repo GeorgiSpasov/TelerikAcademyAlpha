71c625269ed16af43b4df9c7d20b98dd825044c5
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-09 02:22:06 +0300

    Index of Letters

---
+++ 10112.IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _10112.IndexOfLetters
+{
+    class IndexOfLetters
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] letters = new char[26];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = (char)(i + 97);
+            }
+            foreach (char letter in word)
+            {
+                Console.WriteLine(letters.ToList().IndexOf(letter));
+            }
+
+        }
+    }
+}
