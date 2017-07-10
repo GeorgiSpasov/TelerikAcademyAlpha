d1ece71000b14f59c1a9c47fd6f4fd6007ed5663
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-10 13:00:47 +0300

    Reverse Number

---
+++ 10307.ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _10307.ReverseNumber
+{
+    class ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string reversed = Reverser(number);
+            Console.WriteLine(reversed);
+        }
+        public static string Reverser(string number)
+        {
+            string reversed = String.Join("", number.ToArray().Reverse());
+            return reversed;
+        }
+    }
+}
