cd2cbc276b056bc3fd984d2b5b6dc73f715d1364
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-11 02:25:54 +0300

    Sorting Array

---
+++ 10309.SortingArray/SortingArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _10309.SortingArray
+{
+    class SortingArray
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            SortArray(intArray);
+            Console.WriteLine(string.Join(" ", intArray));
+        }
+
+        public static int FindMax(int[] intArray, int startIndex, out int switchIndex)
+        {
+            int max = int.MinValue;
+            switchIndex = startIndex;
+            for (int i = startIndex; i >= 0; i--)
+            {
+                if (max < intArray[i])
+                {
+                    max = intArray[i];
+                    switchIndex = i;
+                }
+            }
+            return max;
+        }
+
+        public static void SortArray(int[] intArray)
+        {
+            int switchdNum;
+            int max;
+            for (int i = intArray.Length - 1; i >= 0; i--)
+            {
+                int switchIndex;
+                switchdNum = intArray[i];
+                max = FindMax(intArray, i, out switchIndex);
+                intArray[switchIndex] = switchdNum;
+                intArray[i] = max;
+            }
+        }
+    }
+}
