62719a325659e757a423bf1a8f845d3d6df342e3
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-10 11:54:53 +0300

    Get Largest Number

---
+++ 10302.GetLargestNumber/GetLargestNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _10302.GetLargestNumber
+{
+    class GetLargestNumber
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            foreach (int num in numbers)
+            {
+                max = GetMax(num, max);
+            }
+            Console.WriteLine(max);
+        }
+        public static int GetMax(int current, int max)
+        {
+            int result;
+            result = Math.Max(current, max);
+            return result;
+        }
+    }
+}
