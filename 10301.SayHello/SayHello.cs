f676b51321d4afcb9e8cc59270989c000edb6d65
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-10 11:38:26 +0300

    Say Hello

---
+++ 10301.SayHello/SayHello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10301.SayHello
+{
+    class SayHello
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Greeting(name);
+        }
+        public static void Greeting(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
