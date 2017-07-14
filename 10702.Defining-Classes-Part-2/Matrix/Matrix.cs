bb286e2059993ff6facd6233ec58e5604b28d7c7
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-15 01:24:39 +0300

    Matrix

---
+++ 10702.Defining-Classes-Part-2/Matrix/Matrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10702.Defining_Classes_Part_2.Matrix
+{
+    public class Matrix<T>
+    {
+
+    }
+}
