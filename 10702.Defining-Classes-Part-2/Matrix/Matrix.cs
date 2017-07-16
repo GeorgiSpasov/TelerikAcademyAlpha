985d40e2a4f7c36c1c0aa28bb2dfd0f29a5f2ad4
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-16 23:54:16 +0300

    Matrix Multiplication

--- 10702.Defining-Classes-Part-2/Matrix/Matrix.cs
+++ 10702.Defining-Classes-Part-2/Matrix/Matrix.cs
@@ -1,31 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _10702.Defining_Classes_Part_2.Matrix
 {
     public class Matrix<T>
     {
-        private T[,] array2D;
+        private T[,] matrix;
 
         public Matrix(int rows, int cols)
         {
-            this.array2D = new T[rows, cols];
+            this.matrix = new T[rows, cols];
         }
 
         public Matrix(T[,] arrayT)
         {
-            this.array2D=arrayT;
+            this.matrix = arrayT;
         }
 
         public int Rows
         {
             get
             {
-                return this.array2D.GetLength(0);
+                return this.matrix.GetLength(0);
             }
         }
 
@@ -33,7 +28,7 @@ namespace _10702.Defining_Classes_Part_2.Matrix
         {
             get
             {
-                return this.array2D.GetLength(1);
+                return this.matrix.GetLength(1);
             }
         }
 
@@ -50,7 +45,7 @@ namespace _10702.Defining_Classes_Part_2.Matrix
                     throw new IndexOutOfRangeException($"Col index {col} is invalid");
                 }
 
-                return this.array2D[row, col];
+                return this.matrix[row, col];
             }
             set
             {
@@ -63,18 +58,18 @@ namespace _10702.Defining_Classes_Part_2.Matrix
                     throw new IndexOutOfRangeException($"Col index {col} is invalid");
                 }
 
-                this.array2D[row, col] = value;
+                this.matrix[row, col] = value;
             }
         }
 
         public static Matrix<T> operator +(Matrix<T> m1, Matrix<T> m2)
         {
-            Matrix<T> result = new Matrix<T>(m1.Rows, m1.Cols);
-
             if (m1.Rows != m2.Rows || m1.Cols != m2.Cols)
             {
                 throw new ArgumentOutOfRangeException("Passed nonmatching matrices!");
             }
+
+            Matrix<T> result = new Matrix<T>(m1.Rows, m1.Cols);
             for (int row = 0; row < m1.Rows; row++)
             {
                 for (int col = 0; col < m1.Cols; col++)
@@ -87,12 +82,12 @@ namespace _10702.Defining_Classes_Part_2.Matrix
 
         public static Matrix<T> operator -(Matrix<T> m1, Matrix<T> m2)
         {
-            Matrix<T> result = new Matrix<T>(m1.Rows, m1.Cols);
-
             if (m1.Rows != m2.Rows || m1.Cols != m2.Cols)
             {
                 throw new ArgumentOutOfRangeException("Passed nonmatching matrices!");
             }
+
+            Matrix<T> result = new Matrix<T>(m1.Rows, m1.Cols);
             for (int row = 0; row < m1.Rows; row++)
             {
                 for (int col = 0; col < m1.Cols; col++)
@@ -103,23 +98,26 @@ namespace _10702.Defining_Classes_Part_2.Matrix
             return result;
         }
 
-        //public static Matrix<T> operator *(Matrix<T> m1, Matrix<T> m2)
-        //{
-        //    Matrix<T> result = new Matrix<T>(m1.Rows, m1.Cols);
+        public static Matrix<T> operator *(Matrix<T> m1, Matrix<T> m2)
+        {
+            if (m1.Cols != m2.Rows)
+            {
+                throw new ArgumentOutOfRangeException("Passed nonmatching matrices!");
+            }
 
-        //    if (m1.Rows != m2.Rows || m1.Cols != m2.Cols)
-        //    {
-        //        throw new ArgumentOutOfRangeException("Passed nonmatching matrices!");
-        //    }
-        //    for (int row = 0; row < m1.Rows; row++)
-        //    {
-        //        for (int col = 0; col < m1.Cols; col++)
-        //        {
-        //            result[row, col] = (dynamic)m1[row, col] - (dynamic)m2[row, col];
-        //        }
-        //    }
-        //    return result;
-        //}
+            Matrix<T> result = new Matrix<T>(m1.Rows, m2.Cols);
+            for (int row = 0; row < result.Rows; row++)
+            {
+                for (int col = 0; col < result.Cols; col++)
+                {
+                    for (int k = 0; k < m1.Cols; k++)
+                    {
+                        result[row, col] += (dynamic)m1[row, k] * (dynamic)m2[k, col];
+                    }
+                }
+            }
+            return result;
+        }
 
     }
 }
--- 10702.Defining-Classes-Part-2/Test.cs
+++ 10702.Defining-Classes-Part-2/Test.cs
@@ -32,40 +32,59 @@ namespace _10702.Defining_Classes_Part_2
             //pointsArray.ClearList();
             //Console.WriteLine(pointsArray + "\n");
 
-            Matrix<int> intMatrix = new Matrix<int>(5, 6);
-            for (int row = 0; row < intMatrix.Rows; row++)
-            {
-                for (int col = 0; col < intMatrix.Cols; col++)
-                {
-                    intMatrix[row, col] = row + col;
-                }
-            }
+            //Matrix<int> intMatrix = new Matrix<int>(5, 6);
+            //for (int row = 0; row < intMatrix.Rows; row++)
+            //{
+            //    for (int col = 0; col < intMatrix.Cols; col++)
+            //    {
+            //        intMatrix[row, col] = row + col;
+            //    }
+            //}
             //PrintMatrix(intMatrix);
 
-            int[,] a1 =
-             {
-                {1, 2, 3, 4, 5 },
-                {1, 2, 3, 4, 5 },
-                {1, 2, 3, 4, 5 }
-            };
-            int[,] a2 =
-             {
-                {10, 10, 10, 10, 10 },
-                {10, 10, 10, 10, 10 },
-                {10, 10, 10, 10, 10 }
-            };
-            Matrix<int> m1 = new Matrix<int>(a1);
-            PrintMatrix(m1);
-            Console.WriteLine();
-            Matrix<int> m2 = new Matrix<int>(a2);
-            PrintMatrix(m2);
-            Console.WriteLine();
-            PrintMatrix(m1 + m2);
-            Console.WriteLine();
-            PrintMatrix(m1 - m2);
-            Console.WriteLine();
+            //int[,] a1 =
+            // {
+            //    {1, 2, 3, 4, 5},
+            //    {1, 2, 3, 4, 5},
+            //    {1, 2, 3, 4, 5}
+            //};
+            //int[,] a2 =
+            // {
+            //    {10, 10, 10, 10, 10},
+            //    {10, 10, 10, 10, 10},
+            //    {10, 10, 10, 10, 10}
+            //};
+            //Matrix<int> m1 = new Matrix<int>(a1);
+            //PrintMatrix(m1);
+            //Console.WriteLine();
+            //Matrix<int> m2 = new Matrix<int>(a2);
+            //PrintMatrix(m2);
+            //Console.WriteLine();
+            //PrintMatrix(m1 + m2);
+            //Console.WriteLine();
+            //PrintMatrix(m1 - m2);
+            //Console.WriteLine();
 
 
+            int[,] b1 =
+             {
+                {10, 10, 10},
+                {10, 10, 10}
+            };
+            int[,] b2 =
+             {
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5}
+            };
+            Matrix<int> n1 = new Matrix<int>(b1);
+            PrintMatrix(n1);
+            Console.WriteLine();
+            Matrix<int> n2 = new Matrix<int>(b2);
+            PrintMatrix(n2);
+            Console.WriteLine();
+            PrintMatrix(n1 * n2);
+            Console.WriteLine();
 
         }
 
