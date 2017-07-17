4a0f40645ce7dd9d436c8aa3ce6cb2492cf7e65e
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-17 12:00:31 +0300

    Odd Lines

---
+++ 10601.OddLines/OddLines.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace _10601.OddLines
+{
+    public class OddLines
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("../../test.txt", false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.WriteLine($"Line #{i}");
+                }
+            }
+
+            using (StreamReader reader = new StreamReader("../../test.txt"))
+            {
+                int lineCounter = 0;
+                while (!reader.EndOfStream)
+                {
+                    if (lineCounter % 2 != 0)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                    else
+                    {
+                        reader.ReadLine();
+                    }
+                    lineCounter++;
+                }
+            }
+        }
+    }
+}
