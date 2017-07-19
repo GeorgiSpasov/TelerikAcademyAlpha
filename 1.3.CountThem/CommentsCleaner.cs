431010de72d6700f9f66f2bfec312722df9d74a8
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-19 17:54:17 +0300

    Comments check added

---
+++ 1.3.CountThem/CommentsCleaner.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._3.CountThem
+{
+    public static class CommentsCleaner
+    {
+        public static void RemoveComments(string inputLine, StringBuilder cleanText, bool isMultiLine = false)
+        {
+            int commentStart = inputLine.IndexOf("/*");
+            int commentEnd = inputLine.IndexOf("*/");
+
+            if (inputLine.StartsWith(@"//") || inputLine.StartsWith("#"))
+            {
+                return;
+            }
+            else if (inputLine.Contains(@"//") && inputLine.IndexOf(@"//") < commentStart) // words //comments...
+            {
+                commentStart = inputLine.IndexOf(@"//");
+                cleanText.Append(" " + inputLine.Remove(commentStart, inputLine.Length - commentStart));
+            }
+            else if (commentStart > -1 && commentEnd > -1) // ==============================================================================
+            {
+                if (inputLine.Contains(@"//")) // comment */ words // comments.... ???
+                {
+
+                }
+                else if (commentStart < commentEnd) // Comment ends on the same line ???????????? */ words // comments.... ???
+                {
+                    // words /* start */ more words /* second */ words /* to second line
+
+
+
+                    while (commentStart > -1 || commentStart < commentEnd) // till commentEnd =-1
+                    {
+                        inputLine = inputLine.Remove(0, commentEnd + "*/".Length);
+                        commentStart = inputLine.IndexOf("/*");
+
+                        if (commentStart < commentEnd)
+                        {
+
+                        }
+                    }
+
+
+
+                    cleanText.Append(" " + inputLine.Remove(commentStart, commentEnd + "*/".Length - commentStart));
+                }
+                else if (commentStart > commentEnd) // First ends and than starts
+                {
+                    // some text*/ not comment /*start of new comment
+                    inputLine = inputLine.Remove(0, commentEnd + "*/".Length);
+                    commentStart = inputLine.IndexOf("/*");
+
+                    cleanText.Append(" " + inputLine.Remove(commentStart, inputLine.Length - commentStart));
+                    RemoveComments(Console.ReadLine(), cleanText, true); // Search comment's end - isMultiLine = true
+                }
+            }//=============================================================================================================================
+            else if (commentStart > -1 && commentEnd < 0) // Starts on this line and ends on next
+            {
+                cleanText.Append(" " + inputLine.Remove(commentStart, inputLine.Length - commentStart));
+                RemoveComments(Console.ReadLine(), cleanText, true); // Search comment's end - isMultiLine = true
+            }
+            else if (commentStart < 0 && commentEnd < 0 && isMultiLine) // Starts on previous line and ends on next line
+            {
+                RemoveComments(Console.ReadLine(), cleanText, true);
+            }
+            else if (commentStart < 0 && commentEnd > -1 && isMultiLine) // Starts on previous line and ends on this line 
+            {
+                cleanText.Append(" " + inputLine.Remove(0, commentEnd + "*/".Length));
+            }
+            else if (commentStart < 0 && commentEnd < 0 && !isMultiLine) // Line without comments
+            {
+                cleanText.Append(" " + inputLine);
+            }
+        }
+
+        public static void GetVariables(StringBuilder cleanText, List<string> variables)
+        {
+            string text = cleanText.ToString();
+            char[] splitters = text.ToArray().Where(c =>
+                        c != '@' &&
+                        c != '_' &&
+                        c != '\\' &&
+                        c != '-' &&
+                        !char.IsLetterOrDigit(c)).ToArray();
+
+            variables.AddRange(text.Split(splitters, StringSplitOptions.RemoveEmptyEntries)
+                .Where(w =>
+                w.StartsWith("@") &&
+                !w.StartsWith(@"\@") &&
+                !w.Contains("-") &&
+                !w.Contains("\\")).ToArray());
+        }
+    }
+}
--- 1.3.CountThem/CountThem.cs
+++ 1.3.CountThem/CountThem.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace _1._3.CountThem
 {
@@ -9,114 +9,112 @@ namespace _1._3.CountThem
     {
         static void Main(string[] args)
         {
-            string inputLine = null;
-            StringBuilder cleanText = new StringBuilder();
+            bool isComment = false;
             List<string> variables = new List<string>();
 
-            while (inputLine != "{!}")
+
+            //using (StreamReader reader = new StreamReader("../../test.txt"))
             {
-                inputLine = Console.ReadLine();
-                RemoveComments(inputLine, cleanText);
+                while (true)
+                {
+                    string inputLine = Console.ReadLine();
+                    //string inputLine = reader.ReadLine();
+                    if (inputLine == "{!}")
+                    {
+                        break;
+                    }
+                    else if (inputLine.StartsWith("//") ||
+                             inputLine.StartsWith("#"))
+                    {
+                        continue;
+                    }
+                    GetVariables(inputLine, variables, ref isComment);
+                }
             }
 
-            Console.WriteLine();
-            Console.WriteLine(cleanText);
-
             #region Print Result
-            GetVariables(cleanText, variables);
-            variables = variables.Distinct().Select(v => v.TrimStart('@')).ToList();
+            variables = variables.Distinct().ToList();
             variables.Sort();
             Console.WriteLine(variables.Count);
             variables.ForEach(Console.WriteLine);
             #endregion
         }
 
-        public static void RemoveComments(string inputLine, StringBuilder cleanText, bool isMultiLine = false)
+        public static void GetVariables(string inputLine, List<string> variables, ref bool isComment)
         {
-            int commentStart = inputLine.IndexOf("/*");
-            int commentEnd = inputLine.IndexOf("*/");
+            string[] splitters = {" ", "%", "&", "(", ")",
+                "+", ".", ",", " / ", ":", ";", "<", "=", ">",
+                "?", "[", "]", "^", "{", "|", "}", "~"};
 
-            if (inputLine.StartsWith(@"//") || inputLine.StartsWith("#"))
+            string[] words = inputLine.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+
+            bool inDoubleQuotes = false;
+            bool inSingleQuotes = false;
+
+
+            foreach (string word in words)
             {
-                return;
-            }
-            else if (inputLine.Contains(@"//") && inputLine.IndexOf(@"//") < commentStart) // words //comments...
-            {
-                commentStart = inputLine.IndexOf(@"//");
-                cleanText.Append(" " + inputLine.Remove(commentStart, inputLine.Length - commentStart));
-            }
-            else if (commentStart > -1 && commentEnd > -1) // ==============================================================================
-            {
-                if (inputLine.Contains(@"//")) // comment */ words // comments.... ???
+                switch (word)
                 {
-
+                    case "/*":
+                        isComment = true;
+                        break;
+                    case "*/":
+                        isComment = false;
+                        break;
+                    case "//":
+                        return;
+                    default:
+                        break;
                 }
-                else if (commentStart < commentEnd) // Comment ends on the same line ???????????? */ words // comments.... ???
+
+                if (isComment)
                 {
-                    // words /* start */ more words /* second */ words /* to second line
+                    continue;
+                }
+                else
+                {
+                    //Console.Write(word + " ");
 
-
-
-                    while (commentStart > -1 || commentStart < commentEnd) // till commentEnd =-1
+                    if (word.StartsWith("\""))
                     {
-                        inputLine = inputLine.Remove(0, commentEnd + "*/".Length);
-                        commentStart = inputLine.IndexOf("/*");
-
-                        if (commentStart < commentEnd)
-                        {
-
-                        }
+                        inDoubleQuotes = true;
+                    }
+                    else if (word.EndsWith("\""))
+                    {
+                        inDoubleQuotes = false;
+                    }
+                    else if (word.StartsWith("\'"))
+                    {
+                        inSingleQuotes = true;
+                    }
+                    else if (word.EndsWith("\'"))
+                    {
+                        inSingleQuotes = false;
                     }
 
-
-
-                    cleanText.Append(" " + inputLine.Remove(commentStart, commentEnd + "*/".Length - commentStart));
+                    if ((word.StartsWith("@") &&
+                        !char.IsNumber(word[1]) &&
+                        !word.Contains("-") &&
+                        !word.Contains("/")) &&
+                        //check string
+                        ((!inSingleQuotes && !inDoubleQuotes) ||
+                        (word.StartsWith("\\@") ||
+                            (!inSingleQuotes ||
+                             !inDoubleQuotes)) ||
+                        (word.StartsWith("\"@") ||
+                               !inSingleQuotes) ||
+                        (word.StartsWith("\'@") ||
+                               !inDoubleQuotes)))
+                    {
+                        variables.Add(word
+                            .TrimStart('\\')
+                            .TrimStart('@')
+                            .TrimEnd('\'')
+                            .TrimEnd('\"'));
+                    }
                 }
-                else if (commentStart > commentEnd) // First ends and than starts
-                {
-                    // some text*/ not comment /*start of new comment
-                    inputLine = inputLine.Remove(0, commentEnd + "*/".Length);
-                    commentStart = inputLine.IndexOf("/*");
-
-                    cleanText.Append(" " + inputLine.Remove(commentStart, inputLine.Length - commentStart));
-                    RemoveComments(Console.ReadLine(), cleanText, true); // Search comment's end - isMultiLine = true
-                }
-            }//=============================================================================================================================
-            else if (commentStart > -1 && commentEnd < 0) // Starts on this line and ends on next
-            {
-                cleanText.Append(" " + inputLine.Remove(commentStart, inputLine.Length - commentStart));
-                RemoveComments(Console.ReadLine(), cleanText, true); // Search comment's end - isMultiLine = true
             }
-            else if (commentStart < 0 && commentEnd < 0 && isMultiLine) // Starts on previous line and ends on next line
-            {
-                RemoveComments(Console.ReadLine(), cleanText, true);
-            }
-            else if (commentStart < 0 && commentEnd > -1 && isMultiLine) // Starts on previous line and ends on this line 
-            {
-                cleanText.Append(" " + inputLine.Remove(0, commentEnd + "*/".Length));
-            }
-            else if (commentStart < 0 && commentEnd < 0 && !isMultiLine) // Line without comments
-            {
-                cleanText.Append(" " + inputLine);
-            }
-        }
-
-        public static void GetVariables(StringBuilder cleanText, List<string> variables)
-        {
-            string text = cleanText.ToString();
-            char[] splitters = text.ToArray().Where(c =>
-                        c != '@' &&
-                        c != '_' &&
-                        c != '\\' &&
-                        c != '-' &&
-                        !char.IsLetterOrDigit(c)).ToArray();
-
-            variables.AddRange(text.Split(splitters, StringSplitOptions.RemoveEmptyEntries)
-                .Where(w =>
-                w.StartsWith("@") &&
-                !w.StartsWith(@"\@") &&
-                !w.Contains("-") &&
-                !w.Contains("\\")).ToArray());
         }
     }
 }
