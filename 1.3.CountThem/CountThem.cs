3aab33607b1f89f42c495d3697d2c173b53d3974
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-21 22:59:38 +0300

    Count Them

--- 1.3.CountThem/CountThem.cs
+++ 1.3.CountThem/CountThem.cs
@@ -10,7 +10,7 @@ namespace _1._3.CountThem
         static void Main(string[] args)
         {
             List<string> variables = new List<string>();
-            bool isComment = false;
+            //bool isComment = false;
 
             while (true)
             {
@@ -24,72 +24,72 @@ namespace _1._3.CountThem
                     continue;
                 }
 
-                //ProcessLine(input, variables);
+                ProcessLine(input, variables); //===================================================
 
-                #region v.2 80/100
-                bool inDoubleQuotes = false; //No multi-line strings
-                bool inSingleQuotes = false;
+                #region v.2 100/100
+                //bool inDoubleQuotes = false; //No multi-line strings
+                //bool inSingleQuotes = false;
 
-                if (input == "{!}")
-                {
-                    break;
-                }
-                else if (input.StartsWith("//") || input.StartsWith("#"))
-                {
-                    continue;
-                }
+                //if (input == "{!}")
+                //{
+                //    break;
+                //}
+                //else if (input.StartsWith("//") || input.StartsWith("#"))
+                //{
+                //    continue;
+                //}
 
-                for (int i = 0; i < input.Length; i++)
-                {
-                    //Comment start & end
-                    if (input[i] == '/' && i < input.Length - 1 && input.Substring(i, 2) == "/*")
-                    {
-                        isComment = true;
-                        i++;
-                    }
-                    else if (input[i] == '*' && i < input.Length - 1 && input.Substring(i, 2) == "*/")
-                    {
-                        isComment = false;
-                        i++;
-                    }
-                    else if (input[i] == '\'' && !isComment)
-                    {
-                        inSingleQuotes = !inSingleQuotes;
-                    }
-                    else if (input[i] == '\"' && !isComment) //String start & end 
-                    {
-                        inDoubleQuotes = !inDoubleQuotes;
-                    }
-                    else if (input[i] == '/' && !inSingleQuotes && !inDoubleQuotes && i < input.Length - 1 && input[i + 1] == '/') //The start of line is handled above && "https://someUrl.com" @varAfterURL
-                    {
-                        break;
-                    }
-                    else if (!isComment && input[i] == '@')
-                    {
-                        if ((inSingleQuotes ^ inDoubleQuotes) && input[i - 1] == '\\')
-                        {
-                            continue;
-                        }
-                        else if ((inSingleQuotes && input[i - 1] == '\"') || (inDoubleQuotes && input[i - 1] == '\'')) // " or ' escaping
-                        {
-                            continue;
-                        }
+                //for (int i = 0; i < input.Length; i++)
+                //{
+                //    //Comment start & end
+                //    if (input[i] == '/' && i < input.Length - 1 && input.Substring(i, 2) == "/*")
+                //    {
+                //        isComment = true;
+                //        i++;
+                //    }
+                //    else if (input[i] == '*' && i < input.Length - 1 && input.Substring(i, 2) == "*/")
+                //    {
+                //        isComment = false;
+                //        i++;
+                //    }
+                //    else if (input[i] == '\'' && !isComment)
+                //    {
+                //        inSingleQuotes = !inSingleQuotes;
+                //    }
+                //    else if (input[i] == '\"' && !isComment) //String start & end 
+                //    {
+                //        inDoubleQuotes = !inDoubleQuotes;
+                //    }
+                //    else if (input[i] == '/' && !inSingleQuotes && !inDoubleQuotes && i < input.Length - 1 && input.Substring(i, 2) == "//") //The start of line is handled above && "https://someUrl.com" @varAfterURL
+                //    {
+                //        break;
+                //    }
+                //    else if (!isComment && input[i] == '@')
+                //    {
+                //        if ((inSingleQuotes ^ inDoubleQuotes) && input[i - 1] == '\\' && input[i - 2] != '\\')
+                //        {
+                //            continue;
+                //        }
+                //        else if ((inSingleQuotes && input[i - 1] == '\"') || (inDoubleQuotes && input[i - 1] == '\'')) // " or ' escaping
+                //        {
+                //            continue;
+                //        }
 
-                        StringBuilder variable = new StringBuilder();
-                        i++;
-                        while (i < input.Length && (char.IsLetterOrDigit(input[i]) || input[i] == '_'))
-                        {
-                            variable.Append(input[i]);
-                            i++;
-                        }
-                        i--;
+                //        StringBuilder variable = new StringBuilder();
+                //        i++;
+                //        while (i < input.Length && (char.IsLetterOrDigit(input[i]) || input[i] == '_'))
+                //        {
+                //            variable.Append(input[i]);
+                //            i++;
+                //        }
+                //        i--;
 
-                        if (variable.Length > 0)
-                        {
-                            variables.Add(variable.ToString());
-                        }
-                    }
-                }
+                //        if (variable.Length > 0)
+                //        {
+                //            variables.Add(variable.ToString());
+                //        }
+                //    }
+                //}
                 #endregion
             }
 
@@ -99,34 +99,29 @@ namespace _1._3.CountThem
             variables.ForEach(Console.WriteLine);
         }
 
-
-        public static void ProcessLine(string inputLine, List<string> variables, bool isComment = false)
+        //v.3
+        public static void ProcessLine(string input, List<string> variables, bool isComment = false)
         {
-            if (inputLine.StartsWith("//") || inputLine.StartsWith("#"))
-            {
-                return;
-            }
-
             int searchIndex = 0;
 
-            if (isComment && inputLine.IndexOf("*/") == -1)
+            if (isComment && input.IndexOf("*/") == -1)
             {
                 ProcessLine(Console.ReadLine(), variables, true);
                 return;
             }
-            else if (isComment && inputLine.IndexOf("*/") != -1)
+            else if (isComment && input.IndexOf("*/") != -1)
             {
                 isComment = false;
-                searchIndex = inputLine.IndexOf("*/") + "*/".Length;
+                searchIndex = input.IndexOf("*/") + "*/".Length;
             }
 
             int[] indices = new int[] {
-                            inputLine.IndexOf("/*", searchIndex),
-                            inputLine.IndexOf("*/", searchIndex),
-                            inputLine.IndexOf("\'", searchIndex),
-                            inputLine.IndexOf("\"", searchIndex),
-                            inputLine.IndexOf("//", searchIndex),
-                            inputLine.IndexOf("@", searchIndex)
+                            input.IndexOf("/*", searchIndex),
+                            input.IndexOf("*/", searchIndex),
+                            input.IndexOf("\'", searchIndex),
+                            input.IndexOf("\"", searchIndex),
+                            input.IndexOf("//", searchIndex),
+                            input.IndexOf("@", searchIndex)
             };
 
             bool inSingleQuotes = false;
@@ -135,12 +130,12 @@ namespace _1._3.CountThem
             while (indices.Max() > -1)
             {
                 searchIndex = indices.OrderBy(a => a).First(a => a > -1);
-                char currentSymbol = inputLine[searchIndex];
+                char currentSymbol = input[searchIndex];
 
                 switch (currentSymbol)
                 {
                     case '/':
-                        if (searchIndex < inputLine.Length - 1 && inputLine[searchIndex + 1] == '/')
+                        if (!inSingleQuotes && !inDoubleQuotes && searchIndex < input.Length - 1 && input.Substring(searchIndex, 2) == "//")
                         {
                             return;
                         }
@@ -148,7 +143,8 @@ namespace _1._3.CountThem
                         {
                             isComment = true;
                             searchIndex += "/*".Length;
-                            if (inputLine.IndexOf("*/", searchIndex) == -1)
+
+                            if (input.IndexOf("*/", searchIndex) == -1)
                             {
                                 ProcessLine(Console.ReadLine(), variables, true);
                                 return;
@@ -162,19 +158,12 @@ namespace _1._3.CountThem
                     case '\'':
                         if (!isComment)
                         {
-                            if (inDoubleQuotes == true && searchIndex < inputLine.Length - 1 && inputLine[searchIndex + 1] == '@')
-                            {
-                                inSingleQuotes = !inSingleQuotes;
-                            }
-                            else if (inDoubleQuotes == false)
-                            {
-                                inSingleQuotes = !inSingleQuotes;
-                            }
+                            inSingleQuotes = !inSingleQuotes;
                         }
                         searchIndex++;
                         break;
                     case '\"':
-                        if (!isComment && searchIndex < inputLine.Length - 1 && inputLine[searchIndex + 1] != '@')
+                        if (!isComment)
                         {
                             inDoubleQuotes = !inDoubleQuotes;
                         }
@@ -185,11 +174,11 @@ namespace _1._3.CountThem
                         {
                             searchIndex++;
                         }
-                        else if ((inSingleQuotes ^ inDoubleQuotes) && inputLine[searchIndex - 1] == '\\')
+                        else if ((inSingleQuotes ^ inDoubleQuotes) && input[searchIndex - 1] == '\\' && input[searchIndex - 2] != '\\')
                         {
                             searchIndex++;
                         }
-                        else if ((inSingleQuotes && inputLine[searchIndex - 1] == '\"') || (inDoubleQuotes && inputLine[searchIndex - 1] == '\''))
+                        else if ((inSingleQuotes && input[searchIndex - 1] == '\"') || (inDoubleQuotes && input[searchIndex - 1] == '\''))
                         {
                             searchIndex++;
                         }
@@ -197,12 +186,11 @@ namespace _1._3.CountThem
                         {
                             StringBuilder variable = new StringBuilder();
                             searchIndex++;
-                            while (searchIndex < inputLine.Length && (char.IsLetterOrDigit(inputLine[searchIndex]) || inputLine[searchIndex] == '_'))
+                            while (searchIndex < input.Length && (char.IsLetterOrDigit(input[searchIndex]) || input[searchIndex] == '_'))
                             {
-                                variable.Append(inputLine[searchIndex]);
+                                variable.Append(input[searchIndex]);
                                 searchIndex++;
                             }
-                            //searchIndex--; - difference with previous --- 80/100
 
                             if (variable.Length > 0)
                             {
@@ -215,12 +203,12 @@ namespace _1._3.CountThem
                 }
 
                 indices = new int[] {
-                            inputLine.IndexOf("/*", searchIndex),
-                            inputLine.IndexOf("*/", searchIndex),
-                            inputLine.IndexOf("\'", searchIndex),
-                            inputLine.IndexOf("\"", searchIndex),
-                            inputLine.IndexOf("//", searchIndex),
-                            inputLine.IndexOf("@", searchIndex)
+                            input.IndexOf("/*", searchIndex),
+                            input.IndexOf("*/", searchIndex),
+                            input.IndexOf("\'", searchIndex),
+                            input.IndexOf("\"", searchIndex),
+                            input.IndexOf("//", searchIndex),
+                            input.IndexOf("@", searchIndex)
                 };
             }
         }
