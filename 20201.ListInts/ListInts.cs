4ae25236fd4e1d6a2a16100057f555dc05afe66f
Georgi Spasov <georgi.spasov@outlook.com>
2017-08-16 17:14:13 +0300

    ListInts

---
+++ 20201.ListInts/ListInts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20201.ListInts
+{
+    class ListInts
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            int input;
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    break;
+                }
+                ints.Add(input);
+            }
+
+            Console.WriteLine("Sum = " + ints.Sum());
+            Console.WriteLine("Average = " + ints.Average());
+        }
+    }
+}
