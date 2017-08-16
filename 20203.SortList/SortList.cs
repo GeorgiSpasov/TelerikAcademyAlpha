e79551bcac3b4254af52a19212f53757b7f294f4
Georgi Spasov <georgi.spasov@outlook.com>
2017-08-16 17:26:48 +0300

    Sort List

---
+++ 20203.SortList/SortList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20203.SortList
+{
+    class SortList
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            int input;
+
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    break;
+                }
+                ints.Add(input);
+            }
+
+            Console.WriteLine("Sorted: " + string.Join(" ", ints.OrderBy(c => c)));
+        }
+    }
+}
