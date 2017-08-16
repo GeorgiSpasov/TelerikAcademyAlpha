064cefb2b9df709bb3dc33ff21ee42d0345cdfe2
Georgi Spasov <georgi.spasov@outlook.com>
2017-08-16 17:20:51 +0300

    Reverse with Stack

---
+++ 20202.ReverseWithStack/ReverseWithStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20202.ReverseWithStack
+{
+    class ReverseWithStack
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
