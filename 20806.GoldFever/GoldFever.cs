b355cb12356ac293aa7b644c5b9d7c29cb1530eb
Georgi Spasov <georgi.spasov@outlook.com>
2017-08-30 16:23:55 +0300

    Gold Fever

---
+++ 20806.GoldFever/GoldFever.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20806.GoldFever
+{
+    class GoldFever
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] prices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            long sum = 0;
+
+            int boughtOunzes = 0;
+            int money = 0;
+
+
+            for (int i = prices.Length - 1; i > 1; i--)
+            {
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
