fc75b371107cbef6fc7f467c7d48dff68fa4c413
Georgi Spasov <georgi.spasov@outlook.com>
2017-09-25 17:43:51 +0300

    Odd or Even

--- Module3/JavaScript/301401.OddOrEven.js
+++ Module3/JavaScript/301401.OddOrEven.js
@@ -1,12 +1,10 @@
 function solve(args) {
-    // your code goes here
     let num = +args[0];
 
-    if(num%2==0){
-        console.log('even ${num}');
+    if (num % 2 == 0) {
+        console.log(`even ${num}`);
     }
     else {
-        console.log('odd ${num}');       
+        console.log(`odd ${num}`);
     }
 }
-solve([12]);
